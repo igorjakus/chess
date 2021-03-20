686a069da9b930221410faeab700cf89da80bfa0
igorjakus <igorjakus@protonmail.com>
2021-03-20 16:53:24 +0100

    Displaying pieces
    style:
    - prettier methods order
    - moved some code to chessboard.py
    
    new:
    - displaying pieces

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
-import pygame
 from sys import exit
+import pygame
+import chessboard
 
 
 class ChessGame():
@@ -11,50 +12,40 @@ class ChessGame():
         self.screen = pygame.display.set_mode((800, 800))
         pygame.display.set_caption("Python Chess - Igor Jakus")
 
-        self.setup_board()
+        # Setting up chessboard
+        self.board = chessboard.ChessBoard()
 
     def run_game(self):
         while True:
             self._check_events()
             self._update_screen()
 
-    def setup_board(self):
-        self.board = []
-        for i in range(8):
-            for j in range(8):
-                self.board.append(Square(i, j))
-
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.handle_quit()
 
-    def _update_screen(self):
-        self.blit_board()
-        pygame.display.flip()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.board.change_sides()
 
     @staticmethod
     def handle_quit():
         exit()
 
-    def blit_board(self):
-        for square in self.board:
+    def _blit_board(self):
+        for square in self.board.chess_board:
             pygame.draw.rect(self.screen, square.color(),
                              ((*square.position()), 100, 100))
 
+    def _blit_pieces(self):
+        for piece in self.board.chess_pieces:
+            self.screen.blit(piece.img, piece.rect)
 
-class Square():
-    """ Class for chess game square represence """
-    def __init__(self, posX, posY):
-        self.is_black = (posX + posY) % 2 == 0
-        self.posX = posX
-        self.posY = posY
-
-    def position(self):
-        return (self.posX * 100, self.posY * 100)
-
-    def color(self):
-        return (125, 135, 150) if self.is_black else (232, 235, 239)
+    def _update_screen(self):
+        self._blit_board()
+        self._blit_pieces()
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
