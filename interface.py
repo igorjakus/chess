81c12900209decc90ecad417454119cc554f9dc3
Igor Jakus <igorjakus@protonmail.com>
2024-05-28 11:32:42 +0200

    Add highlighting checks, light refactor

--- interface.py
+++ interface.py
@@ -6,11 +6,17 @@ from resource_manager import ResourceManager
 
 
 class UserInterface:
-    SQUARE_SIZE = 80
-    SCREEN_SIZE = (8 * SQUARE_SIZE, 8 * SQUARE_SIZE)
+    SQUARE = 80
+    SCREEN_SIZE = (8 * SQUARE, 8 * SQUARE)
+
+    WHITE = (232, 235, 239)
+    BLACK = (125, 135, 150)
+    LIGHT_BLUE = (111, 159, 191)
+    RED = (255, 102, 102)
+    DARK_RED = (136, 8, 8)
 
     def __init__(self, board: chess.Board):
-        self.resources = ResourceManager(UserInterface.SQUARE_SIZE)
+        self.resources = ResourceManager(UserInterface.SQUARE)
 
         pygame.init()
         self.screen = pygame.display.set_mode(UserInterface.SCREEN_SIZE)
@@ -72,7 +78,7 @@ class UserInterface:
             move = chess.Move(self.selected_piece, square)
 
             if self.is_promotion(move):
-                    move.promotion = self._handle_promotion()
+                move.promotion = self._handle_promotion()
 
             if move in self.board.legal_moves:
                 self.board.push(move)
@@ -91,44 +97,35 @@ class UserInterface:
     def update_screen(self):
         """Update the screen with the current state of the board."""
         self._draw_board()
-        self._highlight_moves()
+        self._draw_effects()
         self._draw_pieces()
         pygame.display.flip()
 
     def _draw_board(self):
-        SQUARE = self.SQUARE_SIZE
-        WHITE = (232, 235, 239)
-        BLACK = (125, 135, 150)
-
         for i in range(8):
             for j in range(8):
-                color = BLACK if (i + j) % 2 else WHITE
-                pygame.draw.rect(
-                    self.screen,
-                    color,
-                    (i * SQUARE, j * SQUARE, SQUARE, SQUARE),
-                )
+                color = self.BLACK if (i + j) % 2 else self.WHITE
+                rect = (i * self.SQUARE, j * self.SQUARE, self.SQUARE, self.SQUARE)
+                pygame.draw.rect(self.screen, color, rect)
 
     def _draw_pieces(self):
-        SQUARE = UserInterface.SQUARE_SIZE
         for square in chess.SQUARES:
             piece = self.board.piece_at(square)
             if piece:
                 piece_image = self.resources.piece_images[piece.symbol()]
                 pos_x, pos_y = self._square_to_position(square)
-                rect = pygame.Rect(pos_x, pos_y, SQUARE, SQUARE)
+                rect = pygame.Rect(pos_x, pos_y, self.SQUARE, self.SQUARE)
                 self.screen.blit(piece_image, rect)
 
     def _get_square_under_mouse(self, mouse_x, mouse_y):
         # ekran zaczyna sie od lewego gornego rogu
-        selected_row = 7 - mouse_y // UserInterface.SQUARE_SIZE
-        selected_col = mouse_x // UserInterface.SQUARE_SIZE
+        selected_row = 7 - mouse_y // self.SQUARE
+        selected_col = mouse_x // self.SQUARE
 
         if self.flipped_view:
             selected_row = 7 - selected_row
             selected_col = 7 - selected_col
 
-        print(selected_row, selected_col)
         return chess.square(selected_col, selected_row)
 
     def _square_to_position(self, square):
@@ -137,33 +134,39 @@ class UserInterface:
         if self.flipped_view:
             col, row = 7 - col, 7 - row
 
-        x = col * self.SQUARE_SIZE
-        y = (7-  row) * self.SQUARE_SIZE
+        x = col * self.SQUARE
+        y = (7 - row) * self.SQUARE
         return x, y
 
+    def _draw_effects(self):
+        self._highlight_moves()
+        self._highlight_selected()
+        self._highlight_check()
+
     def _highlight_moves(self):
-        SQUARE = self.SQUARE_SIZE
-        LIGHT_BLUE = (111, 159, 191)
-        RED = (255, 102, 102)
-
-        if self.selected_piece is not None:
-            x, y = self._square_to_position(self.selected_piece)
-            rect = (x, y, SQUARE, SQUARE)
-            pygame.draw.rect(self.screen, LIGHT_BLUE, rect)
-
         for move in self.legal_moves:
             square = move.to_square
             x, y = self._square_to_position(square)
 
             if self.board.is_capture(move): 
-                rect = (x, y, SQUARE, SQUARE)
-                pygame.draw.rect(self.screen, RED, rect)
+                rect = (x, y, self.SQUARE, self.SQUARE)
+                pygame.draw.rect(self.screen, self.RED, rect)
             else:
-                center = (x + SQUARE // 2, y + SQUARE // 2)
-                pygame.draw.circle(self.screen, LIGHT_BLUE, center, SQUARE // 7)
+                center = (x + self.SQUARE // 2, y + self.SQUARE // 2)
+                pygame.draw.circle(self.screen, self.LIGHT_BLUE, center, self.SQUARE // 7)
+
+    def _highlight_selected(self):
+         if self.selected_piece is not None:
+            x, y = self._square_to_position(self.selected_piece)
+            rect = (x, y, self.SQUARE, self.SQUARE)
+            pygame.draw.rect(self.screen, self.LIGHT_BLUE, rect)
 
     def _highlight_check(self):
-        pass
+        if self.board.is_check():
+            king_square = self.board.king(self.board.turn)
+            x, y = self._square_to_position(king_square)
+            rect = (x, y, self.SQUARE, self.SQUARE)
+            pygame.draw.rect(self.screen, self.DARK_RED, rect)
 
     @staticmethod
     def is_promotion(move):
