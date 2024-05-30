626aad9873fe11e05cb497542facbc2b06f0cd95
Igor Jakus <igorjakus@protonmail.com>
2024-05-30 10:32:48 +0200

    Add modes to game

--- main.py
+++ main.py
@@ -8,23 +8,45 @@ class App:
         self.board = chess.Board()
         self.ui = interface.UserInterface(self.board)
         self.engine = RandomEngine(self.board)
-        self.against_engine = True
+
+    def reset(self):
+        self.ui.reset()
 
     def mode_0(self):
-        while not self.board.is_game_over():
-            self.ui.player_move()
-            self.ui.player_move()
+        self.ui.player_move()
+        self.ui.player_move()
 
     def mode_1(self):
-        while not self.board.is_game_over():
-            self.ui.player_move()
-            self.engine.play_move()
+        self.ui.player_move()
+        self.engine.play_move()
+    
+    def mode_2(self):
+        raise NotImplementedError
+        self.engine.play_move()
+        self.engine.play_move()
+        # need to add displaying moves
 
-    def run(self):
-        while not self.board.is_game_over():
-            self.ui.player_move()
-            self.engine.play_move()
+    def run(self, mode):
+        if mode == 0:
+            turn = self.mode_0
+        elif mode == 1:
+            turn = self.mode_1
+        elif mode == 2:
+            turn = self.mode_2
+        else:
+            raise NotImplementedError
+
+
+        while True:
+            while not self.board.is_game_over():
+                turn()
+            
+            while self.board.is_game_over():
+                if mode == 2:
+                    self.reset()
+                else:
+                    self.ui.handle_events()
 
 if __name__ == "__main__":
     app = App()
-    app.run()
+    app.run(mode=1)
