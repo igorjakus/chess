50c7103dc26a7138b94d66139bcd1fe0797ccd68
Igor Jakus <igorjakus@protonmail.com>
2024-05-30 11:42:40 +0200

    Fix modes, improve hemerisation, remove redundant code

--- interface.py
+++ interface.py
@@ -1,7 +1,6 @@
 from sys import exit
 import pygame
 import chess
-import random
 
 from config import Config
 from resource_manager import ResourceManager
@@ -23,7 +22,6 @@ class UserInterface:
         self.selected_piece = None
         self.flipped_view = False
         self.legal_moves = []
-        self.game_over = False
         self.update_screen()
 
     def reset(self):
@@ -31,30 +29,27 @@ class UserInterface:
         self.selected_piece = None
         self.flipped_view = False
         self.legal_moves = []
-        self.game_over = False
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self._quit_game()
+                self.quit_game()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                self._handle_mouse_click()
+                self.__handle_mouse_click()
                 self.update_screen()
             elif event.type == pygame.KEYDOWN:
-                self._handle_keydown(event)
+                self.__handle_keydown(event)
                 self.update_screen()
 
-    def _quit_game(self):
+    def quit_game(self):
         pygame.quit()
         exit()
 
-    def _handle_keydown(self, event):
+    def __handle_keydown(self, event):
         if event.key == pygame.K_SPACE:
             self.flipped_view = not self.flipped_view
         elif event.key == pygame.K_r:
             self.reset()
-        elif event.key == pygame.K_p:
-            self.random_move()
 
     def _select_piece(self, square):
         if self.board.color_at(square) is self.board.turn:
@@ -66,86 +61,76 @@ class UserInterface:
         while not move_made:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self._quit_game()
+                    self.quit_game()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    move_made = self._handle_mouse_click()
+                    move_made = self.__handle_mouse_click()
                 elif event.type == pygame.KEYDOWN:
-                    self._handle_keydown(event)
+                    self.__handle_keydown(event)
                 self.update_screen()
 
-    def _handle_mouse_click(self):
+    def __handle_mouse_click(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        selected_square = self._get_square_under_mouse(mouse_x, mouse_y)
+        selected_square = self.__get_square_under_mouse(mouse_x, mouse_y)
         if self.selected_piece is None:
             self._select_piece(selected_square)
         elif selected_square == self.selected_piece:
-            self._deselect_piece()
+            self.__deselect_piece()
         else:
-            if self._move_piece(selected_square):
-                return True  # Zwróć True, jeśli ruch został wykonany
-        return False  # Zwróć False, jeśli ruch nie został wykonany
-
-    def _move_piece(self, square):
+            if self.__move_piece(selected_square):
+                return True  # move was made
+        return False  # move was not made
+    def __move_piece(self, square):
         move = chess.Move(self.selected_piece, square)
-        if self._is_promotion(move):
+        if self.__is_promotion(move):
             move.promotion = chess.QUEEN
 
         if move in self.board.legal_moves:
             self.board.push(move)
             self.selected_piece = None
             self.legal_moves = []
+            return True  # move was made
+        return False  # move was not made
 
-            if self.board.is_game_over():
-                self.game_over = True
-            return True  # Ruch został wykonany
-        return False  # Ruch nie został wykonany
-
-    def _deselect_piece(self):
+    def __deselect_piece(self):
         self.selected_piece = None
         self.legal_moves = []
 
-    def _is_promotion(self, move):
+    def __is_promotion(self, move):
         piece = self.board.piece_at(move.from_square)
         return piece and piece.piece_type == chess.PAWN and chess.square_rank(move.to_square) in {0, 7}
 
-    def random_move(self):
-        self._deselect_piece()
-        moves = list(self.board.legal_moves)
-        if moves:
-            self.board.push(random.choice(moves))
-
     def update_screen(self):
-        self._draw_board()
-        self._draw_effects()
-        self._draw_pieces()
-        if self.game_over:
-            self._draw_game_over()
+        self.__draw_board()
+        self.__draw_effects()
+        self.__draw_pieces()
+        if self.board.is_game_over():
+            self.__draw_game_over()
         pygame.display.flip()
 
-    def _draw_board(self):
+    def __draw_board(self):
         for i in range(8):
             for j in range(8):
                 color = Config.BLACK if (i + j) % 2 else Config.WHITE
                 rect = (i * Config.SQUARE, j * Config.SQUARE, Config.SQUARE, Config.SQUARE)
                 pygame.draw.rect(self.screen, color, rect)
 
-    def _draw_pieces(self):
+    def __draw_pieces(self):
         for square in chess.SQUARES:
             piece = self.board.piece_at(square)
             if piece:
                 piece_image = self.resources.piece_images[piece.symbol()]
-                pos_x, pos_y = self._square_to_position(square)
+                pos_x, pos_y = self.__square_to_position(square)
                 rect = pygame.Rect(pos_x, pos_y, Config.SQUARE, Config.SQUARE)
                 self.screen.blit(piece_image, rect)
 
-    def _draw_effects(self):
-        self._highlight_moves()
-        self._highlight_selected()
-        self._highlight_check()
+    def __draw_effects(self):
+        self.__highlight_moves()
+        self.__highlight_selected()
+        self.__highlight_check()
 
-    def _highlight_moves(self):
+    def __highlight_moves(self):
         for move in self.legal_moves:
-            x, y = self._square_to_position(move.to_square)
+            x, y = self.__square_to_position(move.to_square)
             if self.board.is_capture(move):
                 rect = (x, y, Config.SQUARE, Config.SQUARE)
                 pygame.draw.rect(self.screen, Config.RED, rect)
@@ -153,20 +138,20 @@ class UserInterface:
                 center = (x + Config.SQUARE // 2, y + Config.SQUARE // 2)
                 pygame.draw.circle(self.screen, Config.LIGHT_BLUE, center, Config.SQUARE // 7)
 
-    def _highlight_selected(self):
+    def __highlight_selected(self):
         if self.selected_piece is not None:
-            x, y = self._square_to_position(self.selected_piece)
+            x, y = self.__square_to_position(self.selected_piece)
             rect = (x, y, Config.SQUARE, Config.SQUARE)
             pygame.draw.rect(self.screen, Config.LIGHT_BLUE, rect)
 
-    def _highlight_check(self):
+    def __highlight_check(self):
         if self.board.is_check():
             king_square = self.board.king(self.board.turn)
-            x, y = self._square_to_position(king_square)
+            x, y = self.__square_to_position(king_square)
             rect = (x, y, Config.SQUARE, Config.SQUARE)
             pygame.draw.rect(self.screen, Config.DARK_RED, rect)
 
-    def _draw_game_over(self):
+    def __draw_game_over(self):
         text = self.font.render(f"GAME OVER {self.board.result()}", True, Config.DARK_RED)
         text_rect = text.get_rect(center=(Config.SCREEN_SIZE[0] // 2, Config.SCREEN_SIZE[1] // 2))
         self.screen.blit(text, text_rect)
@@ -175,7 +160,7 @@ class UserInterface:
         small_text_rect = small_text.get_rect(center=(Config.SCREEN_SIZE[0] // 2, Config.SCREEN_SIZE[1] // 2 + Config.SCREEN_SIZE[1] // 15))
         self.screen.blit(small_text, small_text_rect)
 
-    def _get_square_under_mouse(self, mouse_x, mouse_y):
+    def __get_square_under_mouse(self, mouse_x, mouse_y):
         selected_row = 7 - mouse_y // Config.SQUARE
         selected_col = mouse_x // Config.SQUARE
 
@@ -185,7 +170,7 @@ class UserInterface:
 
         return chess.square(selected_col, selected_row)
 
-    def _square_to_position(self, square):
+    def __square_to_position(self, square):
         row, col = divmod(square, 8)
         if self.flipped_view:
             col, row = 7 - col, 7 - row
@@ -193,3 +178,7 @@ class UserInterface:
         x = col * Config.SQUARE
         y = (7 - row) * Config.SQUARE
         return x, y
+
+    def flip(self):
+        """Flip view so after player move each player see his pieces on the bottom of the board"""
+        self.flipped_view = not self.flipped_view
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import chess
 import interface
+from random import randint
+
 from engines.random_engine import RandomEngine
 
 
@@ -13,19 +15,43 @@ class App:
         self.ui.reset()
 
     def mode_0(self):
+        # Player vs Player
         self.ui.player_move()
+        self.ui.flip()
+        self.ui.update_screen()
+
         self.ui.player_move()
+        self.ui.flip()
+        self.ui.update_screen()
 
     def mode_1(self):
-        self.ui.player_move()
-        self.engine.play_move()
-    
-    def mode_2(self):
-        raise NotImplementedError
-        self.engine.play_move()
-        self.engine.play_move()
-        # need to add displaying moves
+        # Player vs AI
+        if self.player_starts: 
+            self.ui.player_move()
+            self.ui.update_screen()
 
+            self.engine.play_move()
+            self.ui.update_screen()
+        else:
+            self.engine.play_move()
+            self.ui.update_screen()
+
+            self.ui.player_move()
+            self.ui.update_screen()    
+
+    def mode_2(self):
+        # AI vs AI
+        self.engine.play_move()
+        self.ui.update_screen()
+
+        self.engine.play_move()
+        self.ui.update_screen()
+
+    def who_first(self):
+        self.player_starts = bool(randint(0, 1))
+        if not self.player_starts:
+            self.ui.flip() 
+    
     def run(self, mode):
         if mode == 0:
             turn = self.mode_0
@@ -34,18 +60,20 @@ class App:
         elif mode == 2:
             turn = self.mode_2
         else:
-            raise NotImplementedError
-
+            raise ValueError("Invalid mode. Please choose 0 (Player vs Player), 1 (Player vs AI), or 2 (AI vs AI).")
 
         while True:
+            if mode == 1:
+                self.who_first()
+    
             while not self.board.is_game_over():
                 turn()
             
-            while self.board.is_game_over():
-                if mode == 2:
-                    self.reset()
-                else:
-                    self.ui.handle_events()
+            if mode == 2:
+                self.reset()
+            else:
+                self.ui.handle_events()
+
 
 if __name__ == "__main__":
     app = App()
